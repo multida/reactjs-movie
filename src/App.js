debaede1ab4d430a0a1e5fd9a190cde558a79e69
김다예 <dykim@feelway.com>
2022-10-25 18:53:44 +0900

    useEffect를 배우기 전 카운트 다시 실행해봄

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Button from './Button';
-import styles from "./App.module.css";
+import { useState } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div className="App">
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"클릭해보자"} />
+      <h1>{counter}!</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
