8f8a7bac9864141eff4beef0bcccce8831ea821d
multida <wow_da65@naver.com>
2022-10-26 22:52:13 +0900

    useEffect와 deps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  
+  useEffect(() => {
+    console.log("i run only once.");
+  }, []);
+
+  useEffect(() => {
+    // if(keyword !== "" && keyword.length > 6) {
+      
+    // }
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.");
+  }, [keyword, counter]);
+  
   return (
     <div className="App">
+      <input value={keyword} onChange={onChange} type="text" placeholder='Search' />
       <h1>{counter}!</h1>
       <button onClick={onClick}>Click me!</button>
     </div>
