76899b431c3bebe291f19f3368b2701db551132c
multida <wow_da65@naver.com>
2022-10-26 23:00:46 +0900

    useEffect 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
+//useEffect는 두가지 인자를 갖는다. 첫번째는 실행시키고 싶은 코드,  두번째는 dependences?임! -> react.js가 지켜보아야 하는 것들?
 
 function App() {
   const [counter, setValue] = useState(0);
@@ -9,22 +10,22 @@ function App() {
   
   useEffect(() => {
     console.log("i run only once.");
-  }, []);
+  }, []); //[] 배열이 비어있기 때문에 react.js가 지켜볼게 없기 때문에 한번만 실행
 
   useEffect(() => {
     // if(keyword !== "" && keyword.length > 6) {
       
     // }
     console.log("I run when 'keyword' changes.");
-  }, [keyword]);
+  }, [keyword]);//keyword가 변할때만 반응 (콘솔이 찍힘), keyword는 state에 있음
 
   useEffect(() => {
     console.log("I run when 'counter' changes.");
-  }, [counter]);
+  }, [counter]);//counter가 변할때만 반응 (콘솔이 찍힘), counter는 state에 있음
 
   useEffect(() => {
     console.log("I run when keyword & counter changes.");
-  }, [keyword, counter]);
+  }, [keyword, counter]); //베얄이기 때문에 여러개 넣을 수 있음. 둘 중 하나라도 변화하면 감지
   
   return (
     <div className="App">
