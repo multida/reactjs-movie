8e3e7fc36a8698f338f6624c40267795e36c9cdc
김다예 <dykim@feelway.com>
2022-10-27 10:26:26 +0900

    useEffect 조금 공부?

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react'; 
+
+//useEffect는 두개의 argument를 가지는 function
+//첫번째 argument는 우리가 딱 한번만 실행하고 싶은 코드, 두번째는 
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState();
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = () => {}
+
+  console.log('i run all the time');
+
+
+  useEffect(() => {
+    console.log('Call the API . . .');
+  }, []);
   return (
-    <div className="App">
+    <div>
+      <input onChange={onChange} type="text" placeholder='Search here ...' />
       <h1>{counter}!</h1>
       <button onClick={onClick}>Click me!</button>
     </div>
