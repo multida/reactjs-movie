5b3d765e474f2e3274bd18c6945833d8a99baf0c
multida <wow_da65@naver.com>
2022-11-15 22:42:10 +0900

    Datail.js 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,82 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+// import Movie from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
   const getMovie = async () => {
     const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?movie_id={id}`)
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-
-    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
-
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+
+  return (
+    <div>
+      {loading ? (
+        "loading..."
+      ) : (
+        <div className="box">
+          <h1>{movie.title}</h1>
+          <div>
+            <img src={movie.medium_cover_image} />
+          </div>
+          <div style={{ textAlign: "center" }}>
+            rating: {movie.rating} | year:{movie.year} | runtime :{" "}
+            {movie.runtime} | genres:{" "}
+            {movie.genres.map((genre) => genre + ", ")}{" "}
+          </div>
+          <h5>{movie.description_full}</h5>
+        </div>
+      )}
+    </div>
+  );
+  // const { id } = useParams();
+
+  // const [loading, setLoading] = useState(true);
+  // const [movie, setMovie] = useState([]);
+
+  // const getMovie = async () => {
+  //   const json = await (
+  //     await fetch(`https://yts.mx/api/v2/list_movies.json?movie_id={id}`)
+  //   ).json();
+
+  //   console.log(json);
+
+  //   setMovie(json.data.movie);
+  //   setLoading(false);
+  // };
+
+  // useEffect(() => {
+  //   getMovie();
+  // }, []);
+
+  // return (
+  //   <div>
+  //     {loading ? (
+  //       "loading..."
+  //     ) : (
+  //       <div className="box">
+  //         <h1>{movie.title}</h1>
+  //         <div>
+  //           <img src={movie.medium_cover_image} />
+  //         </div>
+  //         <div style={{ textAlign: "center" }}>
+  //           rating: {movie.rating} | year:{movie.year} | runtime :{" "}
+  //           {movie.runtime} | genres:{" "}
+  //           {movie.genres.map((genre) => genre + ", ")}{" "}
+  //         </div>
+  //         <h5>{movie.description_full}</h5>
+  //       </div>
+  //     )}
+  //   </div>
+  //  );
 }
 
 export default Detail;
